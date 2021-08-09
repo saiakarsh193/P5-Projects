3b48d061e4a0124413626718d794ed42e60f5074
saiakarsh193 <sai.akarsh@research.iiit.ac.in>
2021-08-09 19:40:49 +0530

    Car2D: Added Wall Drawing

--- Car2D/sketch.js
+++ Car2D/sketch.js
@@ -2,13 +2,14 @@ let p;
 let w;
 let cx, xy;
 
+let points = [];
+let start = false;
 
 function setup()
 {
   createCanvas(1200, 680);
   cx = width / 2;
   cy = height / 2;
-  w = new Wall();
   p = new Car();
 }
 
@@ -16,10 +17,27 @@ function draw()
 {
   background(200);
   translate(cx, cy);
+  if(!start)
+    return;
   p.input(keyIsDown(87), keyIsDown(83), keyIsDown(65), keyIsDown(68));
   p.update();
   if(p.checkCollisions(w.walls, true))
     p.reset();
   w.draw();
   p.draw();
+}
+
+function mousePressed()
+{
+  if(start)
+    return;
+  mouseX -= cx;
+  mouseY -= cy;
+  if(mouseButton == LEFT)
+    points.push([mouseX, -mouseY]);
+  else
+  {
+    w = new Wall(points);
+    start = true;
+  }
 }
