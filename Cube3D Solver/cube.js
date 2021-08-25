95ef606a6ac6ad59d9c57dfcc9f2f198f2d70579
saiakarsh193 <sai.akarsh@research.iiit.ac.in>
2021-08-25 08:36:11 +0530

    Cube3D Solver: Cleaned code 2.0

--- Cube3D Solver/cube.js
+++ Cube3D Solver/cube.js
@@ -1,28 +1,13 @@
 class Cube
 {
-  constructor(sx, sy, side)
+  constructor()
   {
-    this.sx = -50;
-    this.sy = -50;
-    this.sw = 30;
-    this.ofst = 20;
-    //   5
-    // 3 0 1 2
-    //   4
-    this.orientation = [
-        [5, 1, 4, 3],
-        [5, 2, 4, 0],
-        [5, 3, 4, 1],
-        [5, 0, 4, 2],
-        [0, 1, 2, 3],
-        [2, 1, 0, 3]];
-    this.rotmap = [
-        [[2, 0], [2, 1], [2, 2], [0, 0], [1, 0], [2, 0], [0, 2], [0, 1], [0, 0], [2, 2], [1, 2], [0, 2]],
-        [[2, 2], [1, 2], [0, 2], [0, 0], [1, 0], [2, 0], [2, 2], [1, 2], [0, 2], [2, 2], [1, 2], [0, 2]],
-        [[0, 2], [0, 1], [0, 0], [0, 0], [1, 0], [2, 0], [2, 0], [2, 1], [2, 2], [2, 2], [1, 2], [0, 2]],
-        [[0, 0], [1, 0], [2, 0], [0, 0], [1, 0], [2, 0], [0, 0], [1, 0], [2, 0], [2, 2], [1, 2], [0, 2]],
-        [[2, 0], [2, 1], [2, 2], [2, 0], [2, 1], [2, 2], [2, 0], [2, 1], [2, 2], [2, 0], [2, 1], [2, 2]],
-        [[0, 2], [0, 1], [0, 0], [0, 2], [0, 1], [0, 0], [0, 2], [0, 1], [0, 0], [0, 2], [0, 1], [0, 0]]];
+    this.sx = -50;    // Cube Orientation
+    this.sy = -50;    //     5
+    this.sw = 30;     //   3 0 1 2
+    this.ofst = 20;   //     4
+    this.orientation = [[5, 1, 4, 3], [5, 2, 4, 0], [5, 3, 4, 1], [5, 0, 4, 2], [0, 1, 2, 3], [2, 1, 0, 3]];
+    this.rotmap = [[[2, 0], [2, 1], [2, 2], [0, 0], [1, 0], [2, 0], [0, 2], [0, 1], [0, 0], [2, 2], [1, 2], [0, 2]], [[2, 2], [1, 2], [0, 2], [0, 0], [1, 0], [2, 0], [2, 2], [1, 2], [0, 2], [2, 2], [1, 2], [0, 2]], [[0, 2], [0, 1], [0, 0], [0, 0], [1, 0], [2, 0], [2, 0], [2, 1], [2, 2], [2, 2], [1, 2], [0, 2]], [[0, 0], [1, 0], [2, 0], [0, 0], [1, 0], [2, 0], [0, 0], [1, 0], [2, 0], [2, 2], [1, 2], [0, 2]], [[2, 0], [2, 1], [2, 2], [2, 0], [2, 1], [2, 2], [2, 0], [2, 1], [2, 2], [2, 0], [2, 1], [2, 2]], [[0, 2], [0, 1], [0, 0], [0, 2], [0, 1], [0, 0], [0, 2], [0, 1], [0, 0], [0, 2], [0, 1], [0, 0]]];
     this.sideTocmap = ["G", "O", "B", "R", "W", "Y"];
     this.cTocolormap = {"G": "green", "O": "orange", "B": "blue", "R": "red", "W": "white", "Y": "yellow"};
     this.sideTooffsetmap = [[this.sx, this.sy], [this.sx + (3 * this.sw + this.ofst), this.sy], [this.sx + 2 * (3 * this.sw + this.ofst), this.sy], [this.sx - (3 * this.sw + this.ofst), this.sy], [this.sx, this.sy + (3 * this.sw + this.ofst)], [this.sx, this.sy - (3 * this.sw + this.ofst)]];
@@ -37,9 +22,7 @@ class Cube
       for(let row = 0;row < 3; row ++)
       {
         for(let col = 0;col < 3; col ++)
-        {
           this.cube[side][row][col] = this.sideTocmap[side];
-        }
       }
     }
   }
@@ -73,18 +56,14 @@ class Cube
     this.cube[side][1][2] = temp[0];
     this.cube[side][2][2] = temp[1];
     temp = [this.cube[this.orientation[side][0]][this.rotmap[side][0][0]][this.rotmap[side][0][1]], this.cube[this.orientation[side][0]][this.rotmap[side][1][0]][this.rotmap[side][1][1]], this.cube[this.orientation[side][0]][this.rotmap[side][2][0]][this.rotmap[side][2][1]]];
-    this.cube[this.orientation[side][0]][this.rotmap[side][0][0]][this.rotmap[side][0][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][9][0]][this.rotmap[side][9][1]];
-    this.cube[this.orientation[side][0]][this.rotmap[side][1][0]][this.rotmap[side][1][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][10][0]][this.rotmap[side][10][1]];
-    this.cube[this.orientation[side][0]][this.rotmap[side][2][0]][this.rotmap[side][2][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][11][0]][this.rotmap[side][11][1]];
-    this.cube[this.orientation[side][3]][this.rotmap[side][9][0]][this.rotmap[side][9][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][6][0]][this.rotmap[side][6][1]];
-    this.cube[this.orientation[side][3]][this.rotmap[side][10][0]][this.rotmap[side][10][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][7][0]][this.rotmap[side][7][1]];
-    this.cube[this.orientation[side][3]][this.rotmap[side][11][0]][this.rotmap[side][11][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][8][0]][this.rotmap[side][8][1]];
-    this.cube[this.orientation[side][2]][this.rotmap[side][6][0]][this.rotmap[side][6][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][3][0]][this.rotmap[side][3][1]];
-    this.cube[this.orientation[side][2]][this.rotmap[side][7][0]][this.rotmap[side][7][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][4][0]][this.rotmap[side][4][1]];
-    this.cube[this.orientation[side][2]][this.rotmap[side][8][0]][this.rotmap[side][8][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][5][0]][this.rotmap[side][5][1]];
-    this.cube[this.orientation[side][1]][this.rotmap[side][3][0]][this.rotmap[side][3][1]] = temp[0];
-    this.cube[this.orientation[side][1]][this.rotmap[side][4][0]][this.rotmap[side][4][1]] = temp[1];
-    this.cube[this.orientation[side][1]][this.rotmap[side][5][0]][this.rotmap[side][5][1]] = temp[2];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][0]][this.rotmap[side][0 + i][0]][this.rotmap[side][0 + i][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][9 + i][0]][this.rotmap[side][9 + i][1]];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][3]][this.rotmap[side][9 + i][0]][this.rotmap[side][9 + i][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][6 + i][0]][this.rotmap[side][6 + i][1]];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][2]][this.rotmap[side][6 + i][0]][this.rotmap[side][6 + i][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][3 + i][0]][this.rotmap[side][3 + i][1]];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][1]][this.rotmap[side][3 + i][0]][this.rotmap[side][3 + i][1]] = temp[0 + i];
   }
 
   rotateAntiClock(side)
@@ -99,78 +78,42 @@ class Cube
     this.cube[side][0][0] = temp[1];
     this.cube[side][1][0] = temp[0];
     temp = [this.cube[this.orientation[side][0]][this.rotmap[side][0][0]][this.rotmap[side][0][1]], this.cube[this.orientation[side][0]][this.rotmap[side][1][0]][this.rotmap[side][1][1]], this.cube[this.orientation[side][0]][this.rotmap[side][2][0]][this.rotmap[side][2][1]]];
-    this.cube[this.orientation[side][0]][this.rotmap[side][0][0]][this.rotmap[side][0][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][3][0]][this.rotmap[side][3][1]];
-    this.cube[this.orientation[side][0]][this.rotmap[side][1][0]][this.rotmap[side][1][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][4][0]][this.rotmap[side][4][1]];
-    this.cube[this.orientation[side][0]][this.rotmap[side][2][0]][this.rotmap[side][2][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][5][0]][this.rotmap[side][5][1]];
-    this.cube[this.orientation[side][1]][this.rotmap[side][3][0]][this.rotmap[side][3][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][6][0]][this.rotmap[side][6][1]];
-    this.cube[this.orientation[side][1]][this.rotmap[side][4][0]][this.rotmap[side][4][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][7][0]][this.rotmap[side][7][1]];
-    this.cube[this.orientation[side][1]][this.rotmap[side][5][0]][this.rotmap[side][5][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][8][0]][this.rotmap[side][8][1]];
-    this.cube[this.orientation[side][2]][this.rotmap[side][6][0]][this.rotmap[side][6][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][9][0]][this.rotmap[side][9][1]];
-    this.cube[this.orientation[side][2]][this.rotmap[side][7][0]][this.rotmap[side][7][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][10][0]][this.rotmap[side][10][1]];
-    this.cube[this.orientation[side][2]][this.rotmap[side][8][0]][this.rotmap[side][8][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][11][0]][this.rotmap[side][11][1]];
-    this.cube[this.orientation[side][3]][this.rotmap[side][9][0]][this.rotmap[side][9][1]] = temp[0];
-    this.cube[this.orientation[side][3]][this.rotmap[side][10][0]][this.rotmap[side][10][1]] = temp[1];
-    this.cube[this.orientation[side][3]][this.rotmap[side][11][0]][this.rotmap[side][11][1]] = temp[2];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][0]][this.rotmap[side][0 + i][0]][this.rotmap[side][0 + i][1]] = this.cube[this.orientation[side][1]][this.rotmap[side][3 + i][0]][this.rotmap[side][3 + i][1]];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][1]][this.rotmap[side][3 + i][0]][this.rotmap[side][3 + i][1]] = this.cube[this.orientation[side][2]][this.rotmap[side][6 + i][0]][this.rotmap[side][6 + i][1]];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][2]][this.rotmap[side][6 + i][0]][this.rotmap[side][6 + i][1]] = this.cube[this.orientation[side][3]][this.rotmap[side][9 + i][0]][this.rotmap[side][9 + i][1]];
+    for(let i = 0;i < 3;i ++)
+      this.cube[this.orientation[side][3]][this.rotmap[side][9 + i][0]][this.rotmap[side][9 + i][1]] = temp[0 + i];
   }
 
-  moveU()
+  move(type)
   {
-    this.rotateClock(5);
-  }
-
-  moveUP()
-  {
-    this.rotateAntiClock(5);
-  }
-
-  moveD()
-  {
-    this.rotateClock(4);
-  }
-
-  moveDP()
-  {
-    this.rotateAntiClock(4);
-  }
-
-  moveR()
-  {
-    this.rotateClock(1);
-  }
-
-  moveRP()
-  {
-    this.rotateAntiClock(1);
-  }
-
-  moveL()
-  {
-    this.rotateClock(3);
-  }
-
-  moveLP()
-  {
-    this.rotateAntiClock(3);
-  }
-
-  moveF()
-  {
-    this.rotateClock(0);
-  }
-
-  moveFP()
-  {
-    this.rotateAntiClock(0);
-  }
-
-  moveB()
-  {
-    this.rotateClock(2);
-  }
-
-  moveBP()
-  {
-    this.rotateAntiClock(2);
+    if(type == 'U')
+      this.rotateClock(5);
+    else if(type == 'UP')
+      this.rotateAntiClock(5);
+    else if(type == 'D')
+      this.rotateClock(4);
+    else if(type == 'DP')
+      this.rotateAntiClock(4);
+    else if(type == 'R')
+      this.rotateClock(1);
+    else if(type == 'RP')
+      this.rotateAntiClock(1);
+    else if(type == 'L')
+      this.rotateClock(3);
+    else if(type == 'LP')
+      this.rotateAntiClock(3);
+    else if(type == 'F')
+      this.rotateClock(0);
+    else if(type == 'FP')
+      this.rotateAntiClock(0);
+    else if(type == 'B')
+      this.rotateClock(2);
+    else if(type == 'BP')
+      this.rotateAntiClock(2);
   }
 
   parseMoves(moves)
@@ -178,14 +121,14 @@ class Cube
     let ans = [];
     for(let i = 0;i < moves.length;i ++)
     {
-        if(moves[i] == 'U' || moves[i] == 'D' || moves[i] == 'R' || moves[i] == 'L' || moves[i] == 'F' || moves[i] == 'B')
-            ans.push(moves[i]);
-        else if(moves[i] == "'" || moves[i] == 'P' || moves[i] == '3')
-            ans[ans.length - 1] += 'P';
-        else if(moves[i] == '2')
-            ans.push(ans[ans.length - 1]);
-        else
-            return [];
+      if(moves[i] == 'U' || moves[i] == 'D' || moves[i] == 'R' || moves[i] == 'L' || moves[i] == 'F' || moves[i] == 'B')
+        ans.push(moves[i]);
+      else if(moves[i] == "'" || moves[i] == 'P' || moves[i] == '3')
+        ans[ans.length - 1] += 'P';
+      else if(moves[i] == '2')
+        ans.push(ans[ans.length - 1]);
+      else
+        return [];
     }
     return ans;
   }
@@ -194,31 +137,6 @@ class Cube
   {
     moves = this.parseMoves(moves);
     for(let i = 0;i < moves.length;i ++)
-    {
-        if(moves[i] == 'U')
-            this.moveU();
-        else if(moves[i] == 'UP')
-            this.moveUP();
-        else if(moves[i] == 'D')
-            this.moveD();
-        else if(moves[i] == 'DP')
-            this.moveDP();
-        else if(moves[i] == 'R')
-            this.moveR();
-        else if(moves[i] == 'RP')
-            this.moveRP();
-        else if(moves[i] == 'L')
-            this.moveL();
-        else if(moves[i] == 'LP')
-            this.moveLP();
-        else if(moves[i] == 'F')
-            this.moveF();
-        else if(moves[i] == 'FP')
-            this.moveFP();
-        else if(moves[i] == 'B')
-            this.moveB();
-        else if(moves[i] == 'BP')
-            this.moveBP();
-    }
+      this.move(moves[i]);
   }
 }
