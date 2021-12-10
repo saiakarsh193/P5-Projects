f8ebffb9c6bf0787efbc83769dfc965a1934be5a
saiakarsh193 <sai.akarsh@research.iiit.ac.in>
2021-12-10 19:18:41 +0530

    Radar: Updated overlay

--- Radar/sketch.js
+++ Radar/sketch.js
@@ -1,5 +1,6 @@
 let cx, cy;
 let rad;
+let dt = [];
 
 class Radar
 {
@@ -15,33 +16,107 @@ class Radar
     imageMode(CORNER);
   }
 
-  makeOverlay()
+  makeOverlay(nor=4, delang=10)
   {
     this.overlay = createGraphics(width, height);
+    // angleMode() not working for external canvas
+    // this.overlay.angleMode(DEGREES);
     this.overlay.background(175, 175, 175, 0);
     this.overlay.translate(cx, cy);
+    this.overlay.strokeWeight(2);
+    this.overlay.textSize(10);
+    // inner number of rings (does not count the outer rim)
+    for(let i = 1;i <= nor;i ++)
+    {
+      let r = i * this.radius / (nor + 1);
+      this.overlay.noFill();
+      this.overlay.stroke(0, 255, 0);
+      this.overlay.circle(0, 0, 2 * r);
+      this.overlay.fill(0, 255, 0);
+      this.overlay.noStroke();
+      this.overlay.textAlign(LEFT);
+      this.overlay.text(r, r + 3, 0);
+      this.overlay.textAlign(RIGHT);
+      this.overlay.text(r, -(r + 3), 0);
+    }
+    // outer rim
     this.overlay.noFill();
     this.overlay.stroke(0, 255, 0);
+    this.overlay.strokeWeight(3);
+    this.overlay.circle(0, 0, 2 * this.radius);
+    this.overlay.textAlign(CENTER, CENTER);
+    this.overlay.textSize(15);
+    let del = Math.PI * 2 / 360;
+    let ctr = 0;
+    for(let a = 0;a < 2 * Math.PI - del; a += del)
+    {
+      this.overlay.noFill();
+      this.overlay.stroke(0, 255, 0);
+      this.overlay.strokeWeight(2);
+      this.overlay.line(this.radius * cos(a), this.radius * sin(a), (this.radius + 5) * cos(a), (this.radius + 5) * sin(a));
+      if(ctr % delang == 0)
+      {
+        this.overlay.strokeWeight(3);
+        this.overlay.line(this.radius * cos(a), this.radius * sin(a), (this.radius + 10) * cos(a), (this.radius + 10) * sin(a));
+        this.overlay.fill(0, 255, 0);
+        this.overlay.noStroke();
+        this.overlay.text(ctr, (this.radius + 25) * cos(a), (this.radius + 25) * sin(a));
+      }
+      ctr ++;
+    }
+    // filler
+    this.overlay.noStroke();
+    this.overlay.fill(0, 255, 0, 50);
+    this.overlay.circle(0, 0, 2 * this.radius);
+    this.overlay.noFill();
+    this.overlay.stroke(0, 255, 0, 50);
     this.overlay.strokeWeight(2);
-    for(let r = 100; r <= 2 * this.radius; r += 100)
-      this.overlay.circle(0, 0, r);
+    del = Math.PI * 2 / 12;
+    for(let a = 0;a < 2 * Math.PI - del; a += del)
+    {
+      this.overlay.line(-this.radius * cos(a), -this.radius * sin(a), this.radius * cos(a), this.radius * sin(a));
+    }
   }
 
-  draw()
+  draw(doLine=false)
   {
     image(this.overlay, 0, 0, width, height);
     translate(cx, cy);
-    // stroke(0, 255, 0);
-    // strokeWeight(3);
-    // noFill();
-    // line(0, 0, this.radius * cos(this.current_angle), this.radius * sin(this.current_angle));
+    if(doLine)
+    {
+      stroke(0, 255, 0);
+      strokeWeight(3);
+      noFill();
+      line(0, 0, this.radius * cos(this.current_angle), this.radius * sin(this.current_angle));
+    }
     noStroke();
     for(let i = 0;i < this.segments;i ++)
     {
       fill(0, 255, 0, map(i, 0, this.segments - 1, 200, 0));
       arc(0, 0, 2 * this.radius, 2 * this.radius, this.current_angle - (i + 1) * (this.spread / this.segments), this.current_angle - i * (this.spread / this.segments));
     }
-    this.current_angle += this.angle_rate;
+    this.current_angle = (this.current_angle + this.angle_rate) % 360;
+  }
+}
+
+class Dot
+{
+  constructor(x, y)
+  {
+    this.x = x;
+    this.y = y;
+    this.max_life = int(360 / 0.8);
+    this.life = this.max_life;
+  }
+
+  draw()
+  {
+    noStroke();
+    fill(0, 255, 0, map(this.life, this.max_life, 0, 255, 60));
+    circle(this.x, this.y, 10);
+    this.life --;
+    if(this.life < 0)
+      this.life = this.max_life;
   }
 }
 
@@ -59,4 +134,17 @@ function draw()
 {
   background(0);
   rad.draw();
+  for(let i = dt.length - 1;i >=0;i --)
+  {
+    if(dt[i].life > 0)
+      dt[i].draw();
+    else
+      dt.splice(i, 1);
+  }
+  if(random() > 0.98)
+  {
+    let trad = map(random(), 0, 1, 0, rad.radius);
+    let tang = rad.current_angle;
+    dt.push(new Dot(trad * cos(tang), trad * sin(tang)));
+  }
 }
